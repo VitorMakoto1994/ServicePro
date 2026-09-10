0639f319bd3369762b35fd07b584bb136f411a80
VitorMakoto1994 <168682657+VitorMakoto1994@users.noreply.github.com>
2026-09-09 23:06:55 -0300

    chore(pwa): atualizar cache para servicepro-cache-v12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - ServicePRO Service Worker Otimizado
-const CACHE_NAME = 'servicepro-cache-v11';
+const CACHE_NAME = 'servicepro-cache-v12';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
