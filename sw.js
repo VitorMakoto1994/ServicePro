42c1f9c28daab62d9820868a6b0b362c94644c1c
VitorMakoto1994 <168682657+VitorMakoto1994@users.noreply.github.com>
2026-09-10 07:06:47 -0300

    chore(cache): bump service worker cache para v14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - ServicePRO Service Worker Otimizado
-const CACHE_NAME = 'servicepro-cache-v14';
+const CACHE_NAME = 'servicepro-cache-v15';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
