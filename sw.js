7bb6dd11dbe6e05b031f8ec6248f885106f307cc
VitorMakoto1994 <168682657+VitorMakoto1994@users.noreply.github.com>
2026-09-09 21:28:56 -0300

    chore: bump cache to v5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - ServicePRO Service Worker Otimizado
-const CACHE_NAME = 'servicepro-cache-v4';
+const CACHE_NAME = 'servicepro-cache-v5';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
